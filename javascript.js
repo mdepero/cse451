6d472b7b4c59cdd15702877437f6f4f5837b4013
Matt DePero <deperomm@miamioh.edu>
2016-05-12 03:30:12 -0400

    testing map javascript

--- javascript.js
+++ javascript.js
@@ -189,11 +189,13 @@ function shareKML(kmlURL){
 	    })
 	    .fail(function (err) {
 	        //handle error with err
+	        console.log(err);
 	        bootbox.alert("An API Error Occurred: "+err);
 	    });
 	})
 	.fail(function (err) {
 	    //handle error with err
+	    console.log(err);
 	    bootbox.alert("An OAuth Error Occurred: "+err);
 	});
 }
