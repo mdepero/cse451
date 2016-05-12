518357d2b927b89ad5c16409d97f270229848730
Matt DePero <deperomm@miamioh.edu>
2016-05-12 10:47:23 -0400

    error checking on popups and location servces

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 /* All code by Matt DePero for CSE451 */
 
 function promptAndGetLocation(){
-	if(hasPopUpBlocker() || !checkLocationServices()){
+	if(!checkLocationServices()){
 		return;
 	}
 	if ("geolocation" in navigator) {
@@ -249,44 +249,8 @@ function makeMapFitMarkers() {
 }
 
 
-function hasPopUpBlocker() {
-    var result = false;
-
-    var poppedWindow = window.open('README.md');//test with arbitrary file
-
-    try {
-        if (typeof poppedWindow == 'undefined') {
-            // Safari with popup blocker... leaves the popup window handle undefined
-            result = true;
-        }
-        else if (poppedWindow && poppedWindow.closed) {
-            // This happens if the user opens and closes the client window...
-            // Confusing because the handle is still available, but it's in a "closed" state.
-            // We're not saying that the window is not being blocked, we're just saying
-            // that the window has been closed before the test could be run.
-            result = false;
-        }
-        else if (poppedWindow && poppedWindow.test) {
-            // This is the actual test. The client window should be fine.
-            result = false;
-        }
-        else {
-            // Else we'll assume the window is not OK
-            result = true;
-        }
-
-    } catch (err) {
-        //if (console) {
-        //    console.warn("Could not access popup window", err);
-        //}
-    }
-    if(result){
-    	bootbox.alert('Please allow pop ups to use this project<br/><b>Be sure to mark "always allowed"</b>');
-    }else{
-    	poppedWindow.close();
-    }
-    console.log('Popup blocker status: '+result);
-    return result;
+function alertPopUpBlocker() {
+    bootbox.alert('Please allow pop ups to use this project<br/><b>Be sure to mark "always allowed"</b>');
 }
 
 
